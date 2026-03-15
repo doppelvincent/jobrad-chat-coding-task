ef44938ad068ec14c3158f9cf499222456a18631
Vincent <vincent.vincent@check24.de>
2026-03-15 15:28:16 +0100

    Remove all functions in ChatHub

--- backend/ChatApp.Api/Hubs/ChatHub.cs
+++ backend/ChatApp.Api/Hubs/ChatHub.cs
@@ -7,19 +7,4 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace ChatApp.Api.Hubs;
 
-public class ChatHub(IUserService userService, ISessionService sessionService, ILogger<ChatHub> logger) : Hub, IChatHub
-{
-    public async Task SendMessage(string sessionId, Message message)
-    {
-        await Clients.Group(sessionId).SendAsync(EventTypes.ReceiveMessage, message);
-    }
-
-    public override async Task OnDisconnectedAsync(Exception? exception)
-    {
-        if (exception is not null)
-            logger.LogWarning(exception, "Connection {ConnectionId} disconnected with error", Context.ConnectionId);
-
-        userService.RemoveUser(Context.ConnectionId);
-        await base.OnDisconnectedAsync(exception);
-    }
-}
+public class ChatHub : Hub;
