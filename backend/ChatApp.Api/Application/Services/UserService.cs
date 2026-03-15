7cc1963e40cc6b0dc2cfae1140cb2ae47d706361
Vincent <vincent.vincent@check24.de>
2026-03-15 15:18:58 +0100

    Cleanup unused functions in user service

--- backend/ChatApp.Api/Application/Services/UserService.cs
+++ backend/ChatApp.Api/Application/Services/UserService.cs
@@ -1,41 +1,22 @@
 using ChatApp.Api.Application.Interfaces;
+using ChatApp.Api.Hubs;
 using ChatApp.Api.Infrastructure.Interfaces;
 using ChatApp.Api.Models;
 using ChatApp.Api.Models.Enums;
+using Microsoft.AspNetCore.SignalR;
 
 namespace ChatApp.Api.Application.Services;
 
-public class UserService(IUserRepository userRepository, ILogger<UserService> logger) : IUserService
+public class UserService(IHubContext<ChatHub> hubContext, IUserRepository userRepository, ILogger<UserService> logger) : IUserService
 {
-    public ChatUser RegisterCustomer(string connectionId, string userName)
-    {
-        var customer = new ChatUser { Name = userName, Role = EUserRoles.Customer };
-        userRepository.Add(connectionId, customer);
-
-        logger.LogInformation("Customer '{UserName}' registered on connection {ConnectionId}", userName, connectionId);
-
-        return customer;
-    }
-
-    public ChatUser RegisterAgent(string connectionId, string agentName)
-    {
-        var agent = new ChatUser { Name = agentName, Role = EUserRoles.Agent };
-        userRepository.Add(connectionId, agent);
-
-        logger.LogInformation("Agent '{AgentName}' registered on connection {ConnectionId}", agentName, connectionId);
-
-        return agent;
-    }
-
-    public ChatUser? GetByConnectionId(string connectionId) =>
-        userRepository.GetByConnectionId(connectionId);
-
     public ChatUser? GetByUserId(string userId) =>
         userRepository.GetByUserId(userId);
 
-    public void RegisterExistingUser(string connectionId, ChatUser user)
+    public void RegisterExistingUser(string connectionId, ChatUser user, string sessionId)
     {
         userRepository.Add(connectionId, user);
+        hubContext.Groups.AddToGroupAsync(connectionId, sessionId);
+
         logger.LogInformation("User '{UserName}' registered on connection {ConnectionId}", user.Name, connectionId);
     }
 
