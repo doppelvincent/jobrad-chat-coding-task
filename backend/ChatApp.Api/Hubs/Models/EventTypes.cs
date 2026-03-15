61afb0702599c0839dd5c09bb586c8e65cb408da
Vincent <vincent.vincent@check24.de>
2026-03-15 15:19:42 +0100

    Add AgentJoin event type

--- backend/ChatApp.Api/Hubs/Models/EventTypes.cs
+++ backend/ChatApp.Api/Hubs/Models/EventTypes.cs
@@ -2,5 +2,6 @@ namespace ChatApp.Api.Hubs.Models;
 
 public static class EventTypes
 {
+    public const string AgentJoin = "AgentJoin";
     public const string ReceiveMessage = "ReceiveMessage";
 }
