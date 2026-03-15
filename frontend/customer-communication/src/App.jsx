f9ffece8fb17af95c731036dea88035a2984d06a
Vincent <vincent.vincent@check24.de>
2026-03-15 15:54:48 +0100

    Use /agent for the admin page

--- frontend/customer-communication/src/App.jsx
+++ frontend/customer-communication/src/App.jsx
@@ -2,9 +2,7 @@ import { ChatHubProvider } from "./state/ChatHubProvider.jsx";
 import { CustomerView } from "./components/CustomerView/CustomerView.jsx";
 import { AgentView } from "./components/AgentView/AgentView.jsx";
 
-import "./App.css";
-
-const isAdmin = window.location.pathname === "/admin";
+const isAdmin = window.location.pathname === "/agent";
 
 export default function App() {
     return (
